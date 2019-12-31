87652bff186593b64ab0432b5f34447c3af3fbc8
Mike Gaudreau <gaudreaum01@gmail.com>
2019-12-31 13:06:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+
+
+function map(sourceArray, callbak){
+  let newArray = []
+  for (let i=0; i < sourceArray.length; i++){
+    newArray.push(callbak(sourceArray[i]))
+  }
+  return newArray
+}
+
+
+function reduce(sourceArray, callback, startingPoint=0){
+  let total = !!startingPoint ? startingPoint : sourceArray[0]
+  let i = !!startingPoint ? 0 : 1
+
+  for (; i < sourceArray.length; i++){
+    total = callback(total, sourceArray[i])
+  }
+  return total
+}
