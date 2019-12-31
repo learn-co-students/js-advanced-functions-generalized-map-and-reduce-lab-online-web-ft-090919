a7ef1cacb103814cbe5d69fa17de7590dd5508d0
mmman64 <mcnultymarco@gmail.com>
2019-12-31 13:30:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions here
+const map = (arr, fn) => {
+  let newArr = [];
+
+  for (const el of arr) {
+    newArr.push(fn(el));
+  }
+
+  return newArr;
+};
+
+const reduce = (arr, fn, val) => {
+  let newVal;
+  let i;
+
+  newVal = (!!val) ? val : arr[0];
+  i = (newVal == val ? 0 : 1);
+
+  for (i; i < arr.length; i++) {
+    newVal = fn(arr[i], newVal);
+  }
+
+  return newVal;
+};
