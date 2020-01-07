3108e7e3b7ddd477e2a31c0ac267987e0a9c839f
averagealloy <m.mctighe10@gmail.com>
2020-01-07 11:10:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+
+function map(src,a){
+    let arr = []
+    for (let i = 0; i < src.length; i++){
+        let el = src[i]
+        arr.push(a(el))
+    }
+    return arr
+}
+
+function reduce(src, a, begin){
+    let b = (!!begin) ? begin : src[0]
+    let i = (!!begin) ? 0 : 1
+    for (; i < src.length; i++){
+        b = a(src[i], b)
+    }
+    return b
+}
