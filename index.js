32a7d95d10603275e3cd7cdcdbd9921d25ca53ac
Matthew C <m23.cullen@gmail.com>
2020-01-11 05:17:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+function map(srcRay,callback){
+  let results = [];
+  
+  for(let i = 0; i < srcRay.length; i++){
+    results.push(callback(srcRay[i]));
+  }
+  return results;
+}
+
+function reduce(srcRay,callback,starting){
+  let results = srcRay[0];
+  let i = 1;
+  if(!!starting){
+    results = starting;
+    i--;
+  }
+  
+  for(i; i < srcRay.length; i++){
+    results = callback(srcRay[i], results);
+  }
+  return results;
+}
