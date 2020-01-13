9005500a01e6c67e48df2eb2591c493fb7a17af7
Nightrose2016 <lrbarrer@gmail.com>
2020-01-13 15:24:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+function map(sourceArray,callback){
+  let results = [];
+
+  for(let i = 0; i < sourceArray.length; i++){
+    results.push(callback(sourceArray[i]));
+  }
+  return results;
+}
+
+function reduce(sourceArray,callback,starting){
+  let results = sourceArray[0];
+  let i = 1;
+  if(!!starting){
+    results = starting;
+    i--;
+  }
+
+  for(i; i < sourceArray.length; i++){
+    results = callback(sourceArray[i], results);
+  }
+  return results;
+}
